cb9914b43085d764b4ba7f87a1c92e50fb9b8c66
Sugambaj7 <sugambaj123@gmail.com>
2023-07-27 12:08:12 +0545

    Make use of dial intent and insert intent for making a call and saving a contact

--- app/src/main/java/com/example/simpleinterestcalculator/MainActivity.java
+++ app/src/main/java/com/example/simpleinterestcalculator/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.simpleinterestcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+//import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // Calculator{
@@ -14,25 +17,70 @@ public class MainActivity extends AppCompatActivity {
 //    EditText second_num;
 //    TextView output;}
 
-    EditText name, roll, gender, address;
+    //student_info{
+    //EditText name, roll, gender, address;
+    //}
+
+    EditText phone_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.student_info);
-        name = findViewById(R.id.editTextTextPersonName);
-        roll = findViewById(R.id.editTextNumber);
-        gender = findViewById(R.id.editTextTextGender);
-        address = findViewById(R.id.editTextTextAddress);
+        //student info from here to
+//        setContentView(R.layout.student_info);
+//        name = findViewById(R.id.editTextTextPersonName);
+//        roll = findViewById(R.id.editTextNumber);
+//        gender = findViewById(R.id.editTextTextGender);
+//        address = findViewById(R.id.editTextTextAddress);
+        //here
 
 
-        //setContentView(R.layout.activity_main);{simple interest ko lagi}
+        setContentView(R.layout.callandsavephonenum);
+        phone_num = findViewById(R.id.editTextPhoneNum);
+
+
+
+        //setContentView(R.layout.activity_main);{simple interest ko }
         //simple calculator{
 //        setContentView(R.layout.simple_calculator);
 //         first_num = findViewById(R.id.editTextNumber1);
 //         second_num = findViewById(R.id.editTextNumber2);
 //         output = findViewById(R.id.display_result);}
     }
+
+
+    public void makePhoneCall(View v){
+        String phoneNumberString = phone_num.getText().toString().trim();
+        if(!phoneNumberString.isEmpty()){
+            try{
+                int call_num = Integer.parseInt(phoneNumberString);
+                Intent dial_intent = new Intent(Intent.ACTION_DIAL);
+                dial_intent.setData(Uri.parse("tel:"+ call_num));
+                startActivity(dial_intent);
+            }
+            catch (NumberFormatException e){
+                Toast.makeText(this, "Invalid phone number input.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(this, "Phone number cannot be empty", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void saveNum(View v){
+        String phoneNumberString = phone_num.getText().toString().trim();
+        if(!phoneNumberString.isEmpty()){
+            int call_num = Integer.parseInt(phoneNumberString);
+            Intent insertIntent = new Intent(Intent.ACTION_INSERT);
+            insertIntent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+            insertIntent.putExtra(ContactsContract.Intents.Insert.PHONE, call_num);
+            startActivity(insertIntent);
+        }
+        else{
+            Toast.makeText(this, "Phone number cannot be empty", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
     //Simple Interest{
 //    public void Calculate(View v){
@@ -110,20 +158,21 @@ public class MainActivity extends AppCompatActivity {
 //    }
     //}
 
-    public void displaySecondActivity(View v){
-        Intent intent = new Intent(this, Display_Second_Activity.class);
-
-        String name_student = name.getText().toString();
-        String roll_student = roll.getText().toString();
-        String gender_student = gender.getText().toString();
-        String address_student = address.getText().toString();
-
-        intent.putExtra("name_of_student", name_student);
-        intent.putExtra("roll_of_student", roll_student);
-        intent.putExtra("gender_of_student", gender_student);
-        intent.putExtra("address_of_student", address_student);
-
-        startActivity(intent);
-    }
+    //for student info
+//    public void displaySecondActivity(View v){
+//        Intent intent = new Intent(this, Display_Second_Activity.class);
+//
+//        String name_student = name.getText().toString();
+//        String roll_student = roll.getText().toString();
+//        String gender_student = gender.getText().toString();
+//        String address_student = address.getText().toString();
+//
+//        intent.putExtra("name_of_student", name_student);
+//        intent.putExtra("roll_of_student", roll_student);
+//        intent.putExtra("gender_of_student", gender_student);
+//        intent.putExtra("address_of_student", address_student);
+//
+//        startActivity(intent);
+//    }
 
 }
